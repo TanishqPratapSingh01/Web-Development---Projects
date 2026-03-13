59389b2cc88f3dd4666b0c33cca7009d89967607
pc <prataptanishq01@gmail.com>
2026-03-13 08:43:26 +0530

    Server_Setup

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express(); 
+
+app.post('/api/data',(req,res) => {
+    res.json({message: 'Data received successfully'});
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
